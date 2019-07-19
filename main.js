8bfdc7a17100ca6fce83797a3141ff7316173930
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-19 09:29:55 +0200

    --inseriti i due dropdown menu mancanti--

--- main.js
+++ main.js
@@ -2,14 +2,26 @@ $( document ).ready(function() {
   $(".box-drop.conti > a").click(
     function(){
       $(".drop-conti").toggle(500)
-    })
-    $(".box-drop.prodotti > a").click(
-      function(){
-        $(".drop-prodotti").toggle(500)
-      })
-    $(".box-drop.azienda > a").click(
-      function(){
-        $(".drop-azienda").toggle(500)
-      })
+    });
 
+  $(".box-drop.prodotti > a").click(
+    function(){
+      $(".drop-prodotti").toggle(500)
+    });
+
+  $(".box-drop.azienda > a").click(
+    function(){
+      $(".drop-azienda").toggle(500)
+    });
+
+  $(".box-drop.aiuto > a").click(
+    function(){
+      $(".drop-aiuto").toggle(500)
+    });
+
+  $(".box-drop.lingua > a").click(
+    function(){
+      $(".drop-lingua").toggle(500)
+    });
+    
 });
